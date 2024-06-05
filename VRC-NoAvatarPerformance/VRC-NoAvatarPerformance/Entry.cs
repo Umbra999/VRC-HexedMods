cbcc0bad26de6253d2c4b3427cd4c486ddb1c00e
Umbra <69671761+Umbra999@users.noreply.github.com>
2024-06-05 04:22:34 +0200

    No Performance Scanner

---
+++ VRC-NoAvatarPerformance/VRC-NoAvatarPerformance/Entry.cs
@@ -0,0 +1,12 @@
+﻿using CoreRuntime.Interfaces;
+
+namespace VRC_NoAvatarPerformance
+{
+    public class Entry : HexedCheat
+    {
+        public override void OnLoad(string[] args)
+        {
+            ScannerPatch.ApplyPatch();
+        }
+    }
+}
---
+++ VRC-NoAvatarPerformance/VRC-NoAvatarPerformance/ScannerPatch.cs
@@ -0,0 +1,20 @@
+﻿using CoreRuntime.Manager;
+using VRC.SDKBase.Validation.Performance;
+
+namespace VRC_NoAvatarPerformance
+{
+    internal class ScannerPatch
+    {
+        private delegate IntPtr _GetPerformanceScannerSetDelegate(bool __0);
+
+        public static void ApplyPatch()
+        {
+            HookManager.Detour<_GetPerformanceScannerSetDelegate>(typeof(AvatarPerformance).GetMethod(nameof(AvatarPerformance.GetPerformanceScannerSet)), Patch);
+        }
+
+        private static IntPtr Patch(bool __0)
+        {
+            return IntPtr.Zero;
+        }
+    }
+}
