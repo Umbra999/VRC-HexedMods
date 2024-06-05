964a562ffb8b914cd00c9ea7115e91f1f500ca14
Umbra <69671761+Umbra999@users.noreply.github.com>
2024-06-05 04:10:42 +0200

    HWID Patch

---
+++ VRC-HWIDSpoof/VRC-HWIDSpof/Entry.cs
@@ -0,0 +1,12 @@
+﻿using CoreRuntime.Interfaces;
+
+namespace VRC_HWIDSpoof
+{
+    public class Entry : HexedCheat
+    {
+        public override void OnLoad(string[] args)
+        {
+            SystemInfoPatch.ApplyPatch();
+        }
+    }
+}
