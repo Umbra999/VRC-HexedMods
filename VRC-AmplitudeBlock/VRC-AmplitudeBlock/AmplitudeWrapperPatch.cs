cb781a9ceab8e898c1ce93709b1ac7b2bbcf5560
Umbra <69671761+Umbra999@users.noreply.github.com>
2024-06-11 04:40:17 +0200

    Amplitude Blocker

---
+++ VRC-AmplitudeBlock/VRC-AmplitudeBlock/AmplitudeWrapperPatch.cs
@@ -0,0 +1,24 @@
+﻿using CoreRuntime.Manager;
+using System.Reflection;
+using AmplitudeSDKWrapper;
+
+namespace VRC_AmplitudeBlock
+{
+    internal class AmplitudeWrapperPatch
+    {
+        private delegate void _ReturnAllDelegate();
+
+        public static void ApplyPatch()
+        {
+            foreach (MethodInfo method in typeof(AmplitudeWrapper).GetMethods().Where(x => x.Name.StartsWith("Init") && x.Name != "InitializeDeviceId" || x.Name.Contains("Start") || x.Name.Contains("End") || x.Name.Contains("UpdateServer") || x.Name.Contains("PostEvents") || x.Name.Contains("LogEvent") || x.Name.Contains("SaveEvent") || x.Name.Contains("SaveAndUpload") || x.Name.StartsWith("Set")))
+            {
+                HookManager.Detour<_ReturnAllDelegate>(method, Patch);
+            }
+        }
+
+        private static void Patch()
+        {
+            
+        }
+    }
+}
---
+++ VRC-AmplitudeBlock/VRC-AmplitudeBlock/Entry.cs
@@ -0,0 +1,12 @@
+﻿using CoreRuntime.Interfaces;
+
+namespace VRC_AmplitudeBlock
+{
+    public class Entry : HexedCheat
+    {
+        public override void OnLoad(string[] args)
+        {
+            AmplitudeWrapperPatch.ApplyPatch();
+        }
+    }
+}
